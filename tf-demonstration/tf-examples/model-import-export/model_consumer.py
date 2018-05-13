2d4f680e3f098d9dc33e3ef666b4318e19dd7c42
jp <jpohanka@gmail.com>
2018-05-13 15:57:07 +0200

    Added few comments in the mode_consumer.py code.

--- tf-demonstration/tf-examples/model-import-export/model_consumer.py
+++ tf-demonstration/tf-examples/model-import-export/model_consumer.py
@@ -1,12 +1,19 @@
 import tensorflow as tf
 
+# Import the model TF graph via a Saver class.
 saver = tf.train.import_meta_graph('saved_model/linear_regression.ckpt-1900.meta')
 
 with tf.Session() as sess:
+    
+    # Restore the model.
     saver.restore(sess, 'saved_model/linear_regression.ckpt-1900')
+    
+    # Get references to the input variable 'x_sample' and output variable
+    # 'y_calc' from the TF graph via the 'inputs' collection.
     x_sample = sess.graph.get_collection("inputs")[0]
     y_calc = sess.graph.get_collection("y_calc")[0]
     
+    # Run a test batch calculation with few input values.
     print(sess.run(y_calc, feed_dict = {
      x_sample:[[0.0], [1.0], [2.0], [3.0]],
     }))
